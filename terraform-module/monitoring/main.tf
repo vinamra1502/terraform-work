81c04f69778cce8a9d399e467fefa328bcfd3707
vinamra.gupta <vinamra.gupta@innovaccer.com>
2022-06-21 00:09:12 +0530

    add security hub, cloudwatchrule for security hub

--- terraform-module/monitoring/main.tf
+++ terraform-module/monitoring/main.tf
@@ -497,3 +497,53 @@ resource "aws_guardduty_detector" "alert" {
     }
   }
 }
+resource "aws_cloudwatch_event_rule" "guardduty" {
+  name        = "guarduty-findings-rule"
+  description = "Event rule to trigger SNS notification on medium severity or greater GuardDuty finding"
+  is_enabled  = "true"
+
+
+  event_pattern = <<PATTERN
+{
+      "source": ["aws.guardduty"],
+      "detail-type": ["GuardDuty Finding"],
+      "detail": {
+        "severity": [4,4.0,4.1,4.2,4.3,4.4,4.5,4.6,4.7,4.8,4.9,5,5.0,5.1,5.2,5.3,5.4,5.5,5.6,5.7,5.8,5.9,6,6.0,6.1,6.2,6.3,6.4,6.5,6.6,
+          6.7,6.8,6.9,7,7.0,7.1,7.2,7.3,7.4,7.5,7.6,7.7,7.8,7.9,8,8.0,8.1,8.2,8.3,8.4,8.5,8.6,8.7,8.8,8.9]
+       }
+}
+PATTERN
+}
+resource "aws_cloudwatch_event_target" "guardduty" {
+  rule = aws_cloudwatch_event_rule.guardduty.name
+  target_id = "Target0"
+  arn = aws_sns_topic.this.arn
+}
+resource "aws_securityhub_account" "guardsecurityhub" {
+
+}
+resource "aws_cloudwatch_event_rule" "guardsecurityhub" {
+  name        = "security-hub-imported-findings"
+  description = "Event rule to trigger SNS notification on security hub imported findings"
+  is_enabled  = "true"
+
+
+  event_pattern = <<PATTERN
+{
+    "source": ["aws.securityhub"],
+    "detail-type": ["Security Hub Findings - Imported"],
+    "detail": {
+      "findings": {
+        "Severity": {
+          "Label": ["HIGH", "CRITICAL"]
+         }
+       }
+     }
+}
+PATTERN
+}
+resource "aws_cloudwatch_event_target" "guardsecurityhub" {
+  rule = aws_cloudwatch_event_rule.guardsecurityhub.name
+  target_id = "Target0"
+  arn = aws_sns_topic.this.arn
+}
